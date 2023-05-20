97b146cb76bafe4a4fd5debc8af8df8b3414cb4b
pkolc <piotrkolc00@gmail.com>
2023-05-20 21:34:00 +0200

    addin web manifest v2

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    VitePWA({ 
+    registerType: 'autoUpdate',
+    devOptions: {enabled: true},
+    injectRegister: 'auto'
+    })
+  ],
   base:'/',
 })
