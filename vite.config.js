148716a168864e63564b100310c558667402c34f
pkolc <piotrkolc00@gmail.com>
2023-05-21 16:04:50 +0200

    pwa fix

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,36 @@ export default defineConfig({
     VitePWA({ 
     registerType: 'autoUpdate',
     devOptions: {enabled: true},
-    injectRegister: 'auto'
+    injectRegister: 'auto',
+    workbox: {
+    globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    },
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'E-doctor',
+        short_name: 'e-doc',
+        description: 'spa app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+        },
+    
     })
   ],
   base:'/',
