812f20993e97d732ed9a22f19aa2fd88a936a0f0
pkolc <piotrkolc00@gmail.com>
2023-06-04 14:02:42 +0200

    sw fix v1

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(), 
-    VitePWA({ 
-    registerType: 'autoUpdate',
-    devOptions: {enabled: true},
-    injectRegister: 'auto',
-    workbox: {
-    globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-    },
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
         name: 'E-doctor',
         short_name: 'e-doc',
@@ -24,25 +20,31 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'android-chrome-192x192.png',
+            src: './android-chrome-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
-            src: 'android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'android-chrome-512x512.png',
+            src: './android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-        },
-    
-    })
+          },
+          {
+            src: './android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
   ],
-  base:'/',
+  base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
